9ed21e4d38243005fc82bbabc624a05a27aa9c68
SampoLang <bgu444@myy.haaga-helia.fi>
2022-09-20 16:00:09 +0300

    edit function and some frontend

--- src/main/java/hh/swd20com/example/BookStore/web/BookController.java
+++ src/main/java/hh/swd20com/example/BookStore/web/BookController.java
@@ -17,15 +17,37 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
-	@RequestMapping(value = "/studentlist", method=RequestMethod.GET)
+	@RequestMapping(value = "/booklist", method=RequestMethod.GET)
 	public String bookFrom(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 		}
+	//delete book
 	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
-	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
-		return "redirect:../studentlist";
+		return "redirect:../booklist";
 		
 	}
+	//add new book
+	@RequestMapping(value="/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	//save book
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	//add the chosen book by id to model
+	@RequestMapping(value="/edit/{id}", method = RequestMethod.GET)
+	public String addEditBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		System.out.println(model);
+		return "editbook";
+	}
+	
+
 }
