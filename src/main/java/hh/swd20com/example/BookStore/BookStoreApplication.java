5926fb83705fbcac614992d774c290610157784e
SampoLang <bgu444@myy.haaga-helia.fi>
2022-09-23 12:03:47 +0300

    Added console logging

--- src/main/java/hh/swd20com/example/BookStore/BookStoreApplication.java
+++ src/main/java/hh/swd20com/example/BookStore/BookStoreApplication.java
@@ -1,7 +1,8 @@
 package hh.swd20com.example.BookStore;
 
 
-import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,15 +15,15 @@ import hh.swd20com.example.BookStore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookStoreApplication {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(BookStoreApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository) {
+	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		return (args) -> {
-			
 			Book book1 = new Book("Potter", "Rowling", 2000,"1234hh", 19.90);
 			Book book2 = new Book("LOTR", "Tolkien", 1980,"1234ty", 60.3);
 			Book book3 = new Book("Animal Farm", "Orwell", 1989,"123tut-443", 6.3);
@@ -40,8 +41,18 @@ public class BookStoreApplication {
 			categoryRepository.save(category2);
 			categoryRepository.save(category3);
 			categoryRepository.save(category4);
-			
-		
+			log.info("Save some sample books\n");
+			log.info("Save some sample categories\n");
+			log.info("Fetch all the categories");
+			categoryRepository.findAll().forEach((Category) -> {
+				log.info("{}", Category);
+			});
+			log.info("");
+			log.info("Fetch all the books");
+			bookRepository.findAll().forEach((Book) -> {
+				log.info("{}", Book + "\n");
+				
+			});
 		};
 	}
 
--- src/main/java/hh/swd20com/example/BookStore/domain/Book.java
+++ src/main/java/hh/swd20com/example/BookStore/domain/Book.java
@@ -70,7 +70,7 @@ public class Book {
 	}
 	@Override
 	public String toString() {
-		return "Book [id=" + id + "title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+		return "Book [id=" + id + " title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
 				+ "]";
 	}
 	
